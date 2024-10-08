b4d9fe6bf6ec6815b499bf90464f47a6291a2371
NUTZER-PC\firstcyborg <derkaoui010amine@gmail.com>
2024-10-08 17:31:42 +0200

    njdfids

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello feojbijdsafijsa!");
         System.out.println("nijdsnfjdsfoesfnmojesnfdojes");
+        System.out.println();
     }
 }
